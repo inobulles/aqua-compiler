b457792d8e5a71b601f43b28ef76f83ef2720f1a
Obiwac <obiwac@gmail.com>
2019-08-22 18:19:39 +0200

    Started work on new assembler

--- langs/asm/build.sh
+++ langs/asm/build.sh
@@ -2,8 +2,8 @@
 #!/bin/bash
 set -e
 
-gcc main.c -Wno-builtin-declaration-mismatch #-DVERBOSE_MODE=$1 -DDEBUGGING_MODE=$2 -D__ASM_ZVM_V__=$3
-./a.out
+gcc main.c #-DVERBOSE_MODE=$1 -DDEBUGGING_MODE=$2 -D__ASM_ZVM_V__=$3
+./a.out in code.asm out rom.zed
 mv rom.zed ../../rom.zed
 
 exit 0
