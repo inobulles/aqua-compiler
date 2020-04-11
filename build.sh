352c7bb4612330f5d5fc05fb5bdff38af0c59b19
Obiwac <obiwac@gmail.com>
2020-04-11 23:19:34 +0200

    Started on big restructuring of AQUA

--- build.sh
+++ build.sh
@@ -27,5 +27,32 @@ else
 	exit 1
 fi
 
+echo "Creating package ..."
+rm -rf package
+mkdir package
+echo "zed" > package/start
+mv rom.zed package/rom.zed
+
+if [ -d "code/meta" ]; then
+	echo "Moving meta folder to package ..."
+	mv code/meta package/meta
+else
+	echo "No meta folder found, automatically creating development one ..."
+	mkdir package/meta
+	
+	echo "Development application" > package/meta/name
+	echo "Developer" > package/meta/author
+	echo "development" > package/meta/version
+	echo "Development Company" > package/meta/organization
+	echo "Development application." > package/meta/description
+fi
+
+if [ -d "code/assets" ]; then
+	echo "Moving assets folder to package ..."
+	mv code/assets package/assets
+fi
+
+iar --pack package --output package.zpk
+
 echo "AQUA compiler terminated with no errors"
 exit 0
